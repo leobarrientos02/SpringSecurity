dc6e5fa1d825b24bdd3c90f8af27434745f31034
leobarrientos02 <leobarrientos02@gmail.com>
2022-07-07 19:19:53 -0400

    anyMatchers

--- SpringSecurity/demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ SpringSecurity/demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -13,6 +13,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*")
+                .permitAll() // anyMatchers and permitAll are used to allow pages with no authorization
                 .anyRequest()
                 .authenticated()
                 .and()
