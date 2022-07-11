fd887975675a746ce1e59d26998de67d2b643850
leobarrientos02 <leobarrientos02@gmail.com>
2022-07-10 23:50:23 -0400

    Database Authentication

--- SpringSecurity/demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ SpringSecurity/demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,26 +1,21 @@
 package com.example.demo.security;
 
+import com.example.demo.auth.ApplicationUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
 import static com.example.demo.security.ApplicationUserRole.*;
-import static com.example.demo.security.ApplicationUserPermission.*;
 
 
 @Configuration
@@ -29,10 +24,11 @@ import static com.example.demo.security.ApplicationUserPermission.*;
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
-
+    private final ApplicationUserService applicationUserService;
     @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, ApplicationUserService applicationUserService) {
         this.passwordEncoder = passwordEncoder;
+        this.applicationUserService = applicationUserService;
     }
 
     @Override
@@ -77,29 +73,43 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth)throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
     @Bean
-    protected UserDetailsService userDetailsService() {
-      UserDetails leoBarrientosUser = User.builder()
-                .username("leoBarrientos")
-                .password(passwordEncoder.encode("password"))
-//                .roles(STUDENT.name()) // ROLE_STUDENT
-                .authorities(STUDENT.getGrantedAuthorities())
-                .build();
-
-      UserDetails joseUser = User.builder()
-              .username("joseBarrientos")
-              .password(passwordEncoder.encode("password"))
-//              .roles(ADMIN.name())
-              .authorities(ADMIN.getGrantedAuthorities())
-              .build();
-
-      UserDetails mynorUser = User.builder()
-              .username("mynorPimentel")
-              .password(passwordEncoder.encode("password"))
-//              .roles(ADMINTRAINEE.name())
-              .authorities(ADMINTRAINEE.getGrantedAuthorities())
-              .build();
-
-      return new InMemoryUserDetailsManager(leoBarrientosUser, joseUser, mynorUser );
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder); // Allows password to be encoded
+        provider.setUserDetailsService(applicationUserService);
+        return provider;
     }
 }
+
+/* In Memory Database Implementation
+@Override
+@Bean
+protected UserDetailsService userDetailsService() {
+    UserDetails leoBarrientosUser = User.builder()
+            .username("leoBarrientos")
+            .password(passwordEncoder.encode("password"))
+//                .roles(STUDENT.name()) // ROLE_STUDENT
+            .authorities(STUDENT.getGrantedAuthorities())
+            .build();
+
+    UserDetails joseUser = User.builder()
+            .username("joseBarrientos")
+            .password(passwordEncoder.encode("password"))
+//              .roles(ADMIN.name())
+            .authorities(ADMIN.getGrantedAuthorities())
+            .build();
+
+    UserDetails mynorUser = User.builder()
+            .username("mynorPimentel")
+            .password(passwordEncoder.encode("password"))
+//              .roles(ADMINTRAINEE.name())
+            .authorities(ADMINTRAINEE.getGrantedAuthorities())
+            .build();
+
+    return new InMemoryUserDetailsManager(leoBarrientosUser, joseUser, mynorUser );
+}
+*/
